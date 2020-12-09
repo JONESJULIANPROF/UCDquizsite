327d5a9a8790cff99b487ba7b7f196b0ee30994d
JONESJULIANPROF <56853901+JONESJULIANPROF@users.noreply.github.com>
2020-12-08 21:21:38 -0500

    Week 1 done

--- Functional_Test.py
+++ Functional_Test.py
@@ -1,7 +1,29 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class FunctionalTest(unittest.TestCase):
+    global str
+
+    def setUp(self):
+        chromeOptions = webdriver.ChromeOptions()
+        chromeOptions.headless=True
+
+        self.browser = webdriver.Chrome(options=chromeOptions)
+        self.browser.get('http://localhost:8000')
 
 
+    def testTitleHome(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Home', self.browser.title, 'Wrong title')
+
+    
+    def testTitlePage1(self):
+        self.browser.get('http://localhost:8000/page1.html')
+        self.assertIn('Question 1', self.browser.title, 'Wrong title')
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
--- UCDquizApp/tests.py
+++ UCDquizApp/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
+from django.urls import resolve
+from UCDquizApp.views import home_page
+from UCDquizApp.views import page1_page
+from django.http import HttpRequest
+
+
+class ProgramTest(TestCase):
+    def testHomePage(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page, "Home is bad")
+    
+    def testHomePage(self):
+        found = resolve('/page1.html')
+        self.assertEqual(found.func, page1_page, "page1 is bad")
+
+    def testHomeH1(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<h1>Welcome Traveler!</h1>', html, 'it broke')
+    def testHomeLabel(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<label for="inputBox">What is your name, fair one?</label>', html, 'it broke')
+    def testHomeInput(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<input type="text" id="inputBox">', html, 'it broke')
+    def testHomeButton(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<button id="submit" type="submit" onclick="storeName()">Submit!</button>', html, 'it broke')
+    def testHomeP(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<p id="output"></p>', html, 'it broke')
+
+    def testPageH1(self):
+        request = HttpRequest()
+        response = page1_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<h1 id="banner"></h1>', html, 'it broke')
+    
+    def testPageLabel(self):
+        request = HttpRequest()
+        response = page1_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<label for="inputBox">There is a strange glass on the table, do you take it?</label>', html, 'it broke')
+    
+    def testPageInput(self):
+        request = HttpRequest()
+        response = page1_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<input type="text" id="inputBox">', html, 'it broke')
+    def testPageButton(self):
+        request = HttpRequest()
+        response = page1_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<button type="submit" id="submit">Submit!</button>', html, 'it broke')
+    
+    
+
+
+
 
 # Create your tests here.
+
+
